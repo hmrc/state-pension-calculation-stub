b110e73f34fca3294b959b1a9e4ee49d0cfba890
mac grewal <7713555+macgrewal@users.noreply.github.com>
2019-02-20 16:07:08 +0000

    Added code for basic Play 2.6 microservice

---
+++ build.sbt
@@ -0,0 +1,19 @@
+import uk.gov.hmrc.DefaultBuildSettings.integrationTestSettings
+import uk.gov.hmrc.SbtArtifactory
+import uk.gov.hmrc.sbtdistributables.SbtDistributablesPlugin.publishingSettings
+
+val appName = "state-pension-calculation-stub"
+
+lazy val microservice = Project(appName, file("."))
+  .enablePlugins(play.sbt.PlayScala, SbtAutoBuildPlugin, SbtGitVersioning, SbtDistributablesPlugin, SbtArtifactory)
+  .settings(
+    majorVersion := 0,
+    libraryDependencies ++= AppDependencies.compile ++ AppDependencies.test,
+    dependencyOverrides ++= AppDependencies.overrides
+  )
+  .settings(publishingSettings: _*)
+  .configs(IntegrationTest)
+  .settings(integrationTestSettings(): _*)
+  .settings(resolvers += Resolver.jcenterRepo)
+  .settings(CodeCoverageSettings.settings: _*)
+
---
+++ project/AppDependencies.scala
@@ -0,0 +1,39 @@
+import play.core.PlayVersion.current
+import sbt._
+
+object AppDependencies {
+
+  val compile: Seq[ModuleID] = Seq(
+    "uk.gov.hmrc" %% "bootstrap-play-26" % "0.36.0"
+  )
+
+  val test: Seq[ModuleID] = Seq(
+    "com.typesafe.play" %% "play-test" % current % "test",
+    "org.scalatest" %% "scalatest" % "3.0.5" % "test",
+    "org.scalamock" %% "scalamock" % "4.1.0" % "test",
+    "org.scalatestplus.play" %% "scalatestplus-play" % "3.1.2" % "test, it",
+    "org.pegdown" % "pegdown" % "1.6.0" % "test, it",
+    "com.github.tomakehurst" % "wiremock" % "2.20.0" % "test, it"
+  )
+
+  // Fixes a transitive dependency clash between wiremock and scalatestplus-play
+  val overrides: Set[ModuleID] = {
+    val jettyFromWiremockVersion = "9.2.24.v20180105"
+    Set(
+      "org.eclipse.jetty" % "jetty-client" % jettyFromWiremockVersion,
+      "org.eclipse.jetty" % "jetty-continuation" % jettyFromWiremockVersion,
+      "org.eclipse.jetty" % "jetty-http" % jettyFromWiremockVersion,
+      "org.eclipse.jetty" % "jetty-io" % jettyFromWiremockVersion,
+      "org.eclipse.jetty" % "jetty-security" % jettyFromWiremockVersion,
+      "org.eclipse.jetty" % "jetty-server" % jettyFromWiremockVersion,
+      "org.eclipse.jetty" % "jetty-servlet" % jettyFromWiremockVersion,
+      "org.eclipse.jetty" % "jetty-servlets" % jettyFromWiremockVersion,
+      "org.eclipse.jetty" % "jetty-util" % jettyFromWiremockVersion,
+      "org.eclipse.jetty" % "jetty-webapp" % jettyFromWiremockVersion,
+      "org.eclipse.jetty" % "jetty-xml" % jettyFromWiremockVersion,
+      "org.eclipse.jetty.websocket" % "websocket-api" % jettyFromWiremockVersion,
+      "org.eclipse.jetty.websocket" % "websocket-client" % jettyFromWiremockVersion,
+      "org.eclipse.jetty.websocket" % "websocket-common" % jettyFromWiremockVersion
+    )
+  }
+}
